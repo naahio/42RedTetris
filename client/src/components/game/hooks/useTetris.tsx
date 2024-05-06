53c517e1e707643db4ca9a9acb5c251e54d55960
Mohamed Babela <mbabela@e2r4p14.1337.ma>
2024-05-06 16:27:57 +0100

    add practice game

--- client/src/components/game/hooks/useTetris.tsx
+++ client/src/components/game/hooks/useTetris.tsx
@@ -122,7 +122,7 @@ enum TickSpeed {
   
       let isPressingLeft = false;
       let isPressingRight = false;
-      let moveIntervalID: number | undefined;
+      let moveIntervalID: NodeJS.Timeout | undefined;
   
       const updateMovementInterval = () => {
         clearInterval(moveIntervalID);
