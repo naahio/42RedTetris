991e8941a4235563ddf06c4b93242ff2b3f9d6f8
4reha <ael-hadd@student.1337.ma>
2024-05-08 09:22:18 +0100

    Renaming files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import AboutPage from './pages/AboutPage';
-import HomePage from './pages/HomePage';
+import ContactPage from './pages/Contact';
+import AboutPage from './pages/About';
+import HomePage from './pages/Home';
 import Layout from './layout/Layout';
 // import Game from './components/game/GameBoard'
 import Queue from './pages/game/Queue'
