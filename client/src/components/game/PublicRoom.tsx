7d67c0025402d129196b5f870791f0a2dcb1fa07
4reha <ael-hadd@student.1337.ma>
2024-05-13 18:30:08 +0100

    refactor: Add Button and Loader components, update PublicRoom and LeaderBoard components

--- client/src/components/game/PublicRoom.tsx
+++ client/src/components/game/PublicRoom.tsx
@@ -1,44 +1,44 @@
-import axios, { AxiosResponse } from "axios";
-import { Room } from "../../interfaces/Data";
-import { useEffect, useState } from "react";
+import axios, { AxiosResponse } from 'axios';
+import { Room } from '../../interfaces/data';
+import { useEffect, useState } from 'react';
 
 function PublicRoom() {
-    const [rooms, setRooms] = useState<Room[]>([]);
+  const [rooms, setRooms] = useState<Room[]>([]);
 
-    useEffect(() => {
-        const fetchRooms = async () => {
-            try {
-                const response: AxiosResponse = await axios.get('http://localhost:3004/Room');
-                if (response.status === 200) {
-                    setRooms(response.data);
-                }
-            } catch (error) {
-                console.log(error);
-            }
-        };
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const response: AxiosResponse = await axios.get('http://localhost:3004/Room');
+        if (response.status === 200) {
+          setRooms(response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-        fetchRooms();
+    fetchRooms();
 
-        return () => {
-        };
-    }, []);
+    return () => {};
+  }, []);
 
-    return (
-        <div className="">
-            {
-                rooms.map((room: Room) => (
-                    <div key={room.id} className="flex flex-row justify-around bg-darkBlue3 m-5 rounded-lg w-[80%]">
-                        <div className="flex flex-col w-[80%] text-[15px] m-1">
-                            <h2>ROOM ID : {room.uuID}</h2>
-                            <h2>Master : #{room.master}</h2>
-                            <p className="text-lightGray text-opacity-20 text-sm">waiting : {room.members}</p>
-                        </div>
-                        <button className="border w-[30%] bg-joinBlue self-center m-2 rounded-lg">join</button>
-                    </div>
-                ))
-            }
+  return (
+    <div className="">
+      {rooms.map((room: Room) => (
+        <div
+          key={room.id}
+          className="flex flex-row justify-around bg-darkBlue3 m-5 rounded-lg w-[80%]"
+        >
+          <div className="flex flex-col w-[80%] text-[15px] m-1">
+            <h2>ROOM ID : {room.uuID}</h2>
+            <h2>Master : #{room.master}</h2>
+            <p className="text-lightGray text-opacity-20 text-sm">waiting : {room.members}</p>
+          </div>
+          <button className="border w-[30%] bg-joinBlue self-center m-2 rounded-lg">join</button>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default PublicRoom;
